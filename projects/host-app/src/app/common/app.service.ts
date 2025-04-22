5465b5c9c2b95d75b5a0767e4258a5e962a5b052
Mane <vemane@deloitte.com>
2025-04-22 17:10:38 +0530

    Added user login & sign up form

---
+++ projects/host-app/src/app/common/app.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+    providedIn: 'root',
+})
+export class AppService {
+    private baseUrl = 'http://localhost:8001/auth/';
+
+    constructor(private http: HttpClient) {}
+
+    saveUser(endpoint: string, data: any): Observable<any> {
+        return this.http.post(`${this.baseUrl}${endpoint}`, data, {
+            headers: new HttpHeaders({ 'Content-Type': 'application/json' }),
+        });
+    }
+}
+
--- projects/host-app/src/app/home/home.component.ts
+++ projects/host-app/src/app/home/home.component.ts
@@ -1,11 +1,53 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, NgModule } from '@angular/core';
+import { User } from './../common/app.user';
+import { FormsModule } from '@angular/forms';
+import { AppService } from '../common/app.service';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
+
+  constructor(private appService: AppService) { }
+  
+  isSignInVisible: boolean = true;
+  user: User = {
+    email: '',
+    firstName: '',
+    lastName: '',
+    password: '',
+    confirmPassword: ''
+  };
+
+  toggleAuthView(): void {
+    this.isSignInVisible = !this.isSignInVisible;
+  }
+
+  saveUser(): void {
+    const newUser: User = {
+      email: this.user.email,
+      firstName: this.user.firstName,
+      lastName: this.user.lastName,
+      password: this.user.password,
+      confirmPassword: this.user.confirmPassword
+    };
+    if (this.user.password !== this.user.confirmPassword) {
+      alert('Passwords do not match!');
+    }
+    this.appService.saveUser('signup', newUser).subscribe(
+      (response) => {
+        alert('User saved successfully!');
+      },
+      (error) => {
+        alert('Failed to save user!');
+        console.error(error);
+      }
+    );
+  }
+
 
 }
